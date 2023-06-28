fd1a7e5e31ccc1ea0fd7b85e213348580019a6fe
qaz6750 <2838632319@qq.com>
2023-06-29 02:14:27 +0800

    Tools: GCC5: Using X86 version of GCC5 (arm-linux-gnueabihf)
    Signed-off-by: qaz6750 <2838632319@qq.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-##export PACKAGES_PATH=$PWD/edk2:$PWD
-##export WORKSPACE=$PWD/workspace
-##export GCC5_ARM_PREFIX=$PWD/GCC5/bin/arm-linux-gnueabihf-
-##. edk2/edksetup.sh
+pushd edk2
+  git submodule init;git submodule update
+popd
 
-## build -a ARM -p Lumia930Pkg/Lumia930.dsc -t GCC5 -j10 -s -n 0
+export PACKAGES_PATH=$PWD/edk2:$PWD
+export WORKSPACE=$PWD/workspace
+export GCC5_ARM_PREFIX=$PWD/tools/GCC5/bin/arm-linux-gnueabihf-
+. edk2/edksetup.sh
+
+build -a ARM -p Lumia930Pkg/Lumia930.dsc -t GCC5 -j10 -s -n 0
 
 $GCC5_ARM_PREFIX\objcopy -I binary -O elf32-littlearm --binary-architecture arm workspace/build/Lumia930-ARM/DEBUG_GCC5/FV/MSM8974_EFI.fd MSM8974_EFI.fd.elf  
 $GCC5_ARM_PREFIX\ld MSM8974_EFI.fd.elf -T Lumia930Pkg/FvWrapper.ld -o emmc_appsboot.mbn
